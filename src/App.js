bf47f9d630775f8741d86417722929f2081e8e07
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-08-30 20:01:32 +0200

    Added react router package, routes, and simple/test layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+
+import Crew from './pages/Crew';
+import Destination from './pages/Destination';
+import Home from './pages/Home';
+import Layout from './components/layout/Layout';
+import Technology from './pages/Technology';
+
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
