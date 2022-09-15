fa4c2a25f83fbf64a778a9cb5babdbc23b32b8e2
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-09-15 20:47:33 +0200

    Removed unused line-eslint warnings

--- src/components/layout/nav/Nav.jsx
+++ src/components/layout/nav/Nav.jsx
@@ -1,12 +1,11 @@
 import Logo from '../../../assets/shared/logo.svg';
 import Navlinks from './Navlinks';
-import classes from './Nav.module.css';
 
 const Nav = () => {
   return (
     <nav>
       <img src={Logo} alt="" />
-      
+
       <Navlinks />
     </nav>
   );
