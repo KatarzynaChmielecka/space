3bc02ba771c62c346efc04478ec639cda053a288
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-13 16:27:44 +0200

    Improvements in layout, removed comments

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,24 +7,10 @@ const Layout = () => {
   return (
     <>
       <Nav />
-      <div
-        style={{
-          height: '100vh',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <main
-          className={classes.layout}
-          // style={{width:'100%', maxWidth:'1440px', display:'flex', justifyContent:'flex-end', paddingTop:'215px',  backgroundImage:'url("../src/assets/home/background-home-mobile.jpg")',
-          // backgroundSize: "cover",
-          // backgroundRepeat: "no-repeat"}}
-          style={{ width: '100%', maxWidth: '1440px' }}
-        >
-          <Outlet />
-        </main>
-      </div>
+
+      <main className={classes.layout}>
+        <Outlet />
+      </main>
     </>
   );
 };
