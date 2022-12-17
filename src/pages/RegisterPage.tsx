164dcf1819eef720e7ca75111ac541597d6886a9
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-12-17 19:07:59 +0100

    Second test  of register form with image upload

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,47 +1,73 @@
 import * as Yup from 'yup';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useState } from 'react';
 
-import ImageUpload from '../components/ImageUpload';
 import classes from './RegisterPage.module.css';
+
+// import ImageUpload from '../components/ImageUpload';
+
+// import { useFormikContext } from 'formik';
 
 // import { useState } from 'react';
 
-interface RegisterFormValues {
-  username: string;
-  email: string;
-  password: string;
-  image: string;
-}
+// interface RegisterFormValues {
+//   username: string;
+//   email: string;
+//   password: string;
+//   image: string | null
+// }
 
+const initialValues = {
+  username: '',
+  email: '',
+  password: '',
+  image: '',
+};
 const RegisterFormSchema = Yup.object({
   username: Yup.string().min(2, 'Too Short!').required('Required'),
   email: Yup.string().email('Invalid email').required('Required'),
   password: Yup.string().min(8, 'Too Short!').required('Required'),
-  image: Yup.string().required('Requiredaaa'),
+  image: Yup.mixed().required('Requiredaaa'),
 });
 const RegisterPage: React.FC = () => {
-  //   const [userData, setUserData] = useState();
-  //   const handleSubmit = (values: { file: any }) => {
-  //     if (values.file) {
-  //       onFileUpload(values.file);
-  //     }
-  //   };
+  const [userData, setUserData] = useState(initialValues);
+
+  const [imagePreviewUrl, setImagePreviewUrl] = useState('');
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const file = event.target.files![0];
+
+    const reader = new FileReader();
+
+    console.log(file);
+    console.log(event.target.value);
+    reader.onloadend = () => {
+      setImagePreviewUrl(reader.result as string);
+    };
+    // values.image = event?.target.value;
+    // console.log(Formik.children)
+    reader.readAsDataURL(file);
+    setUserData({ ...userData, image: event.target.value });
+    console.log(userData);
+  };
+  // console.log(initialValues);
   return (
     <div
       className={classes['register-page-wrapper']}
       style={{ width: '100%', outline: '1px solid green' }}
     >
       <Formik
-        initialValues={{
-          username: '',
-          email: '',
-          password: '',
-          image: '',
-        }}
+        initialValues={userData}
         validationSchema={RegisterFormSchema}
-        onSubmit={(values: RegisterFormValues) => {
-          // submit form logic goes here
-          console.log(values);
+        onSubmit={() => {
+          // const formData = new FormData();
+          // formData.append('username', values.username);
+          // formData.append('email', values.email);
+          // formData.append('password', values.password);
+          // formData.append('image', values.image);
+          // values.image = event?.target.value;
+          // console.log(formData);
+          console.log(userData);
         }}
       >
         <Form>
@@ -57,7 +83,9 @@ const RegisterPage: React.FC = () => {
           <Field name="password" type="password" />
           <ErrorMessage name="password" />
 
-          <Field name="file" component={ImageUpload} />
+          {/* <ImageUpload name="image" /> */}
+          <input name="image" type="file" onChange={handleImageChange} />
+          {imagePreviewUrl && <img src={imagePreviewUrl} alt="Preview" />}
           <button type="submit">Submit</button>
         </Form>
       </Formik>
