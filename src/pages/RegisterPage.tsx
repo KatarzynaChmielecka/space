b3e942240a656c28ea3997d0b76bde4bee3c3b2d
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-12-28 21:30:59 +0100

    Improved register logic-removed try catch block, removed comments, sorted imports

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,13 +1,14 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import * as Yup from 'yup';
 import axios from 'axios';
 import { ChangeEvent, useState } from 'react';
+import { ToastContentProps, toast } from 'react-toastify';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import classes from './RegisterPage.module.css';
-import { toast, ToastContentProps } from 'react-toastify';
-// import { useNavigate } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
+
 interface RegisterFormValues {
   username: string;
   email: string;
@@ -42,9 +43,7 @@ const RegisterFormSchema = Yup.object({
 
 const RegisterPage: React.FC = () => {
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  // const [error, setError] = useState<string | null>(null);
-  // const [okResponse, setOkResponse] = useState<string | boolean | null>(null);
-  // const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -73,70 +72,43 @@ const RegisterPage: React.FC = () => {
       formData.set(key, value);
     });
 
-    try {
-      const response = await toast.promise(
-        axios.post(
-          `${process.env.REACT_APP_BACKEND_URL}/user/signup`,
-          formData,
-        ),
-        {
-          pending: 'Please, wait.',
-          success: {
-            render() {
-              return <p>{response.data.message} </p>;
-            },
-          },
-          error: {
-            render({
-              data,
-            }: ToastContentProps<{
-              response: { status: number; data: { message: string } };
-              status: number;
-            }>) {
-              if (data && data.response && data?.response.status === 0) {
-                return (
-                  <p>
-                    Sorry, we have problem with database connection. Please try
-                    again later.
-                  </p>
-                );
-              }
-              if (data && data.response && data.response.data) {
-                return <p>{data.response.data.message} </p>;
-              }
-              return <p>Something went wrong, please try again later.</p>;
-            },
+    const response = await toast.promise(
+      axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/signup`, formData),
+      {
+        pending: 'Please, wait.',
+        success: {
+          render() {
+            setPreviewUrl(null);
+            reset();
+            return <p>{response.data.message} </p>;
           },
         },
-        { position: 'top-center' },
-      );
-
-      // if (response.status === 201) {
-      //   console.log(response);
-      //   // setOkResponse(response.data.message);
-      //   navigate('/');
-      // } else {
-      //   toast.error('buuuuuuuu', { position: 'top-center' });
-      //   setError('Something went wrong');
-      //   setOkResponse(false);
-      //   reset();
-      // }
-      console.log(response);
-      setPreviewUrl(null);
-      reset();
-    } catch (error) {
-      // if (axios.isAxiosError(error)) {
-      //   setError(error.response?.data.message);
-      //   setOkResponse(false);
-      // }
-      setPreviewUrl(null);
-      // if (!axios.isAxiosError(error)) {
-      //   toast.error('Something went wrong. Please try again later.');
-      // }
-      toast.error('Something went wrong. Please try again later.');
-      reset();
-      // setError('Something went wrong. Please try again later.');
-    }
+        error: {
+          render({
+            data,
+          }: ToastContentProps<{
+            response: { status: number; data: { message: string } };
+            status: number;
+          }>) {
+            setPreviewUrl(null);
+            reset();
+            if (data && data.response && data?.response.status === 0) {
+              return (
+                <p>
+                  Sorry, we have problem with database connection. Please try
+                  again later.
+                </p>
+              );
+            }
+            if (data && data.response && data.response.data) {
+              return <p>{data.response.data.message} </p>;
+            }
+            return <p>Something went wrong, please try again later.</p>;
+          },
+        },
+      },
+      { position: 'top-center' },
+    );
   });
 
   return (
@@ -145,9 +117,6 @@ const RegisterPage: React.FC = () => {
         onSubmit={onSubmit}
         className={classes['register-page-wrapper__form']}
       >
-        {/* error and okResponse will be toasts */}
-        {/* {error && <p>{error}</p>} */}
-        {/* {okResponse && <p>{okResponse}</p>} */}
         <fieldset>
           <div className={classes['field-wrapper']}>
             <div className={classes['input-wrapper']}>
@@ -206,7 +175,10 @@ const RegisterPage: React.FC = () => {
               <input
                 type="file"
                 {...register('avatar')}
-                onChange={handleImageChange}
+                onChange={(e) => {
+                  handleImageChange(e);
+                  register('avatar').onChange(e);
+                }}
                 name="avatar"
                 className={classes['custom-file-input']}
                 title=""
