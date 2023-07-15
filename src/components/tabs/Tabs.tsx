2985b0ff1303dfb48cb12d42eed138242f9b51fe
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2023-07-15 13:35:11 +0200

    Added logout function

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,14 +1,65 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { ToastContentProps, toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
+import Modal from '../Modal';
 import UserData from './UserData';
 import UserImages from './UserImages';
 import classes from './Tabs.module.css';
+import { AuthContext } from '../../context/auth-context';
 
 const Tabs: React.FC = () => {
   const [activeTab, setActiveTab] = useState('my data');
-
+  const { logout, token } = useContext(AuthContext);
+  const navigate = useNavigate();
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
+  };
+  const handleLogout = async () => {
+    const response = await toast.promise(
+      axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/user/logout`,
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      ),
+      {
+        pending: 'Please, wait.',
+        success: {
+          render() {
+            logout();
+            navigate(`/`);
+            return <p>{response.data.message} </p>;
+          },
+        },
+        error: {
+          render({
+            data,
+          }: ToastContentProps<{
+            response: { status: number; data: { message: string } };
+            status: number;
+          }>) {
+            if (data && data.response && data?.response.status === 0) {
+              return (
+                <p>
+                  Sorry, we have problem with database connection. Please try
+                  again later.
+                </p>
+              );
+            }
+            if (data && data.response && data.response.data) {
+              return <p>{data.response.data.message} </p>;
+            }
+            return <p>Something went wrong, please try again later.</p>;
+          },
+        },
+      },
+      { position: 'top-center' },
+    );
   };
 
   return (
@@ -42,7 +93,7 @@ const Tabs: React.FC = () => {
           NOTES
         </button>
 
-        <button>
+        <button onClick={handleLogout}>
           {' '}
           <span>03</span>LOG OUT
         </button>
@@ -50,23 +101,34 @@ const Tabs: React.FC = () => {
 
       <hr />
 
-      <div className={classes['tabs-content']}>
-        {activeTab === 'my data' && <UserData />}
+      {token && (
+        <div className={classes['tabs-content']}>
+          {activeTab === 'my data' && <UserData />}
 
-        {activeTab === 'photos' && <UserImages />}
+          {activeTab === 'photos' && <UserImages />}
 
-        {activeTab === 'notes' && (
-          <div>
-            <h2>Notes</h2>
+          {activeTab === 'notes' && (
+            <div>
+              <h2>Notes</h2>
 
-            <ul>
-              <li>Note 1</li>
-              <li>Note 2</li>
-              <li>Note 3</li>
-            </ul>
-          </div>
-        )}
-      </div>
+              <ul>
+                <li>Note 1</li>
+                <li>Note 2</li>
+                <li>Note 3</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+
+      {!token && (
+        <Modal
+          title="Something went wrong"
+          content={"You aren't allowed to be here. Please log in againaaa."}
+          modalOnClick={false}
+          showModal={true}
+        />
+      )}
     </div>
   );
 };
